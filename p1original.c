63d7aca297f1f2b30944de737e1925c7fe29656f
Meidingu <94587606+Meidingu@users.noreply.github.com>
2022-02-01 12:15:19 +0000

    ..

--- p1original.c
+++ p1original.c
@@ -1,2 +1,28 @@
+#include<stdio.h>
 
+int input(int *a,int *b)
+{
+  printf("Enter the value of a =\n");
+  scanf("%d",a);
+  printf("Enter the value of b =\n");
+  scanf("%d",b);
+}
+
+int add(int a,int b,int *sum)
+{
+  *sum = a + b;
+}
+
+int output(int a,int b,int sum)
+{
+  printf("Sum = %d\n",sum);
+}
+
+int main()
+{
+  int x,y,sum;
+  input(&x,&y);
+  add(x,y,&sum);
+  output(x,y,sum);
+  return 0;
 }
